97edc38310c5a552dd84f569e90cc26e9e92a391
jogeuncheol <blueqwerty@naver.com>
2021-09-26 00:06:19 +0900

    FIX reset_data, Add game_souls contour_array_delete

--- SOULS_DC.py
+++ SOULS_DC.py
@@ -4,7 +4,6 @@ import tkinter
 import threading
 import cv2
 import numpy as np
-#pyinstaller --icon=icon.ico --onefile --noconsole DSDCX.py
 
 lower_red1 = (0, 67, 67)
 upper_red1 = (5, 255, 255)
@@ -15,12 +14,13 @@ with open("유다희.txt", "w") as f:
     f.write('0')
 
 you_died = False
+death_count = 0
 key = 0
 global frames
 
 
 class UI(tkinter.Tk):
-    death_count = 0
+    global death_count
     program_quit = 0
     __counting_status = 0
 
@@ -30,7 +30,7 @@ class UI(tkinter.Tk):
 
         t_label = tkinter.Label(self, text = "YOU DIED ", font = ("times", "16"))
         t_label.place(x = 50, y = 260)
-        self.d_label = tkinter.IntVar(self, self.death_count)
+        self.d_label = tkinter.IntVar(self, death_count)
         count_label = tkinter.Label(self, textvariable = self.d_label, font = ("times", "16"))
         count_label.place(x=200, y=260)
 
@@ -44,12 +44,16 @@ class UI(tkinter.Tk):
         button4 = tkinter.Button(self, text="SEKIRO", width=17, command=self.start_skr_cap)
         button4.place(x=0, y=353)
         button5 = tkinter.Button(self, text="RESET", width= 17, command=self.reset_data)
-        button5.place(x=130, y= 353)
+        button5.place(x=130, y=353)
 
     def die_counter(self):
-        self.d_label.set(self.death_count)
+        self.d_label.set(death_count)
         print("die_counter fn : ", self.d_label.get())
         self.after(1000, self.die_counter)
+
+    def reset_data(self):
+        global death_count
+        death_count = 0
 
     def start_cap(self):
         global key
@@ -61,7 +65,7 @@ class UI(tkinter.Tk):
             frames = [tkinter.PhotoImage(file="C:/Users/blueq/PycharmProjects/SKRDC/icon1_save3.gif",
                                          format="gif -index %i" % (i)).subsample(2) for i in range(21)]
             self.after(100, draw_gif, 0)
-            death_count_t = death_count_start("souls", self.death_count, self.program_quit)
+            death_count_t = DeathCountStart("souls", self.program_quit)
             death_count_t.daemon = True
             death_count_t.start()
 
@@ -75,7 +79,7 @@ class UI(tkinter.Tk):
             frames = [tkinter.PhotoImage(file="C:/Users/blueq/PycharmProjects/SKRDC/icon1_save3.gif",
                                          format="gif -index %i" % (i)).subsample(2) for i in range(21)]
             self.after(100, draw_gif, 0)
-            death_count_t = death_count_start("sekiro", self.death_count, self.program_quit)
+            death_count_t = DeathCountStart("sekiro", self.program_quit)
             death_count_t.daemon = True
             death_count_t.start()
 
@@ -90,16 +94,11 @@ class UI(tkinter.Tk):
             key = 0
             self.__counting_status = 0
 
-    def reset_data(self):
-        if self.death_count != 0:
-            self.death_count = 0
 
-
-class death_count_start(threading.Thread):
-    def __init__(self, game_type, death_count, program_quit):
+class DeathCountStart(threading.Thread):
+    def __init__(self, game_type, program_quit):
         threading.Thread.__init__(self)
         self.game_type = game_type
-        self.death_count = death_count
         self.program_quit = program_quit
 
     def run(self):
@@ -112,38 +111,21 @@ class death_count_start(threading.Thread):
             img_mask = cv2.inRange(img_hsv, lower_red1, upper_red1)
             img_mask2 = cv2.inRange(img_hsv, lower_red2, upper_red2)
             img_mask3 = img_mask + img_mask2
-            img_result = cv2.bitwise_and(img_ori, img_ori, mask=img_mask3)
+            # img_result = cv2.bitwise_and(img_ori, img_ori, mask=img_mask3)
             contours, _ = cv2.findContours(img_mask3, cv2.RETR_EXTERNAL, cv2.CHAIN_APPROX_SIMPLE)
 
             # 사각형 박스
             fX = []
-            # centers = []
             D = []
             contour_array = []
-            bluebox = []
+            blue_box = []
             for cnt in contours:
                 x, y, w, h = cv2.boundingRect(cnt)  # 수직인 사각형 생성
                 if self.game_type == "souls" and 70 < h < 140 and 10 < w < 110:
                     contour_array.append([x, y, w, h])
-                    # M = cv2.moments(cnt)
-                    # try:
-                    #     cX = int(M['m10'] / M['m00'])
-                    #     cY = int(M['m01'] / M['m00'])
-                    #     centers.append([cX, cY])
-                    #     cv2.rectangle(img_result, (x, y), (x + w, y + h), (0, 255, 0), 3)
-                    # except ZeroDivisionError:
-                    #     print('######')
                 elif self.game_type == "sekiro" and 200 < h < 350 and 50 < w < 350:
                     contour_array.append([x, y, w, h])
-                    # M = cv2.moments(cnt)
-                    # try:
-                    #     cX = int(M['m10'] / M['m00'])
-                    #     cY = int(M['m01'] / M['m00'])
-                    #     centers.append([cX, cY])
-                    #     cv2.rectangle(img_result, (x, y), (x + w, y + h), (0, 255, 0), 3)
-                    # except ZeroDivisionError:
-                    #     print('######')
-            # print(len(contour_array))
+                # __ sort countours
             for i in range(len(contour_array) - 1):
                 index = i
                 for j in range(i + 1, len(contour_array)):
@@ -151,32 +133,59 @@ class death_count_start(threading.Thread):
                     if contour_array[index][0] > contour_array[j][0]:
                         index = j
                 contour_array[i], contour_array[index] = contour_array[index], contour_array[i]
-            # print(contour_array)
-            sum = 0
+            # __ y position average
+            contour_y_sum = 0
             avg = 0
+            if self.game_type == "souls":
+                delete_array = []
+                for i in range(len(contour_array)):
+                    contour_y_sum = contour_y_sum + contour_array[i][1]
+                try:
+                    avg = int(contour_y_sum / len(contour_array))
+                except ZeroDivisionError:
+                    print('######')
+                print("avg : ", avg)
+                for i in range(len(contour_array)):
+                    # __ contour_array[i][1] : contours y position
+                    sub = avg - contour_array[i][1]
+                    if sub < 0: # sub = int(np.sqrt(sub * sub))
+                        sub *= -1
+                    if sub > 80:
+                        delete_array.append(i)
+                for i in reversed(delete_array):
+                    del contour_array[i]
+                delete_array.clear()
+
+            contour_y_sum = 0
             for i in range(len(contour_array)):
-                sum = sum + contour_array[i][1]
+                contour_y_sum = contour_y_sum + contour_array[i][1]
             try:
-                avg = int(sum / len(contour_array))
+                avg = int(contour_y_sum / len(contour_array))
             except ZeroDivisionError:
                 print('######')
-
+            print("len contour_array : ", len(contour_array))
+            print("contour_array : ", contour_array)
             for i in range(len(contour_array)):
+                print("i : ", i)
+                # __ (average - contour_y position) < 50 ? bluebox.append : next
                 sub = avg - contour_array[i][1]
-                sub = int(np.sqrt(sub * sub))
+                if sub < 0: # sub = int(np.sqrt(sub * sub))
+                    sub *= -1
                 if sub < 50:
-                    bluebox.append(contour_array[i])
-                    cv2.rectangle(
-                        img_result,
-                        (contour_array[i][0], contour_array[i][1]),
-                        (contour_array[i][0] + contour_array[i][2], contour_array[i][1] + contour_array[i][3]),
-                        (255, 0, 0), 2)
+                    blue_box.append(contour_array[i])
+                    # __debug : check bounding box
+                    # cv2.rectangle(
+                    #     img_result,
+                    #     (contour_array[i][0], contour_array[i][1]),
+                    #     (contour_array[i][0] + contour_array[i][2], contour_array[i][1] + contour_array[i][3]),
+                    #     (255, 0, 0), 2)
                 # ### contour 사이의 거리[D]를 구함 & contour 들의 수직 비율이 일정해야 함.
-            if len(bluebox) >= 2:  # ##if len(centers) >= 2: if len(contour_array) >= 2:
-                for idx in range(len(bluebox) - 1):
-                    dx = bluebox[idx][0] - bluebox[idx + 1][0]
-                    dy = bluebox[idx][1] - bluebox[idx + 1][1]
+            if len(blue_box) >= 2:  # ##if len(centers) >= 2: if len(contour_array) >= 2:
+                for idx in range(len(blue_box) - 1):
+                    dx = blue_box[idx][0] - blue_box[idx + 1][0]
+                    dy = blue_box[idx][1] - blue_box[idx + 1][1]
                     D.append(int(np.sqrt(dx * dx + dy * dy)))
+            print("D : ", D)
             for i in range(len(D)):
                 if self.game_type == "souls" and D[i] < 160: # __ souls
                     fX.append(D[i])
@@ -187,8 +196,10 @@ class death_count_start(threading.Thread):
                 self.save_death_count()
             elif self.game_type == "sekiro" and len(fX) == 1: # __ sekiro
                 self.save_death_count()
-            if key == 0:
-                break
+            contour_array.clear()
+            blue_box.clear()
+            D.clear()
+            fX.clear()
 
             # __OCR test__
             # text_img = pytesseract.image_to_string(img_result, lang='eng')
@@ -202,11 +213,10 @@ class death_count_start(threading.Thread):
             #     break
 
     def save_death_count(self):
-        self.death_count += 1
-        UI.death_count = self.death_count
-        # print("class UI() private variable : ", UI.death_count)
+        global death_count
+        death_count += 1
         with open("유다희.txt", "w") as f:
-            f.write(str(self.death_count))
+            f.write(str(death_count))
         sleep(5)
 
 
@@ -215,8 +225,6 @@ window.title("유다희")
 window.geometry("260x390+0+0")
 frames = [tkinter.PhotoImage(file="C:/Users/blueq/PycharmProjects/SKRDC/icon1_save3.gif",
                              format="gif -index %i" %(i)).subsample(2) for i in range(1)]
-# frames = tkinter.PhotoImage(file = "C:/Users/blueq/PycharmProjects/DSDXC/icon1.png").subsample(2)
-
 
 def draw_gif(idx):
     try:
@@ -227,10 +235,6 @@ def draw_gif(idx):
     except Exception:
         idx = 0
         window.after(0, draw_gif, idx)
-
-# def draw_png(ind):
-#     gif_label.configure(image=frames)
-#     window.after(100, draw_png, ind)
 
 
 gif_label = tkinter.Label(window)
